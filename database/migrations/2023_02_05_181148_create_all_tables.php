a5fd3e52a30c13ba6344ce51482efa79f0da34f3
Luiz Henrique Santos <luiz.santos85@gmail.com>
2023-02-05 15:47:52 -0300

    migration central

--- database/migrations/2023_02_05_181148_create_all_tables.php
+++ database/migrations/2023_02_05_181148_create_all_tables.php
@@ -13,9 +13,46 @@ class CreateAllTables extends Migration
      */
     public function up()
     {
-        Schema::create('all_tables', function (Blueprint $table) {
+        Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
+            $table->string('email',100);
+            $table->string('password',200);
+            $table->string('name', 100);
+            $table->date('birthdate');
+            $table->string('city', 100)->nullable();
+            $table->string('work', 100)->nullable();
+            $table->string('avatar', 100)->default('avatar.jpg');
+            $table->string('cover', 100)->default('cover.jpg');
+            $table->string('token', 200)->nullable();
+        });
+
+        Schema::create('users_relations', function (Blueprint $table) {
+            $table->id();
+            $table->integer('user_from');
+            $table->integer('user_to');
+        });
+
+        Schema::create('posts', function (Blueprint $table) {
+            $table->id();
+            $table->string('type',20);
+            $table->text('body');
+            $table->dateTime('created_at');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+        });
+
+        Schema::create('posts_likes', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('created_at');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
+        });
+
+        Schema::create('posts_comments', function (Blueprint $table) {
+            $table->id();
+            $table->dateTime('created_at');
+            $table->text('body');
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
         });
     }
 
@@ -26,6 +63,10 @@ class CreateAllTables extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('all_tables');
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('users_relations');
+        Schema::dropIfExists('posts');
+        Schema::dropIfExists('posts_likes');
+        Schema::dropIfExists('posts_comments');
     }
 }
