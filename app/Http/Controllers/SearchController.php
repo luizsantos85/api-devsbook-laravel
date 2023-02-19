27eb630b77cd6dae24c74bb18cc7abe72beb8534
Luiz Henrique Santos <luiz.santos85@gmail.com>
2023-02-19 17:49:57 -0300

    implementano busca de usuario por nome

--- app/Http/Controllers/SearchController.php
+++ app/Http/Controllers/SearchController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 
@@ -17,6 +18,22 @@ class SearchController extends Controller
 
     public function search(Request $request)
     {
+        if (!$request->txt) {
+            return response()->json(['error' => 'Dado não enviado.'], 400);
+        }
+
+        //Busca de usuarios
+        $userList = User::where('name', 'LIKE',"%{$request->txt}%")->select('id','name','avatar')->get();
+        if(!$userList){
+            return response()->json(['error' => 'Nenhum usuário encontrado.'], 400);
+        }
+
+        foreach ($userList as $user) {
+            $user->avatar = url('media/avatars/' . $user->avatar);
+        }
         
+        //Busca de post
+
+        return response()->json(['userList' => $userList], 200);
     }
 }
